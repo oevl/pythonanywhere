59f3daebd11d690e5029a0d6a3d18de903f1f0ff
Omar Vasquez Lima <omar@vasquezlima.com>
2020-12-16 01:58:34 +0000

    Minimal bottle app to serve my .plan file

--- site/bottle_app.py
+++ site/bottle_app.py
@@ -1,10 +1,17 @@
 
 # A very simple Bottle Hello World app for you to get started with...
 from bottle import default_app, route
+homeMsg = "Soon oevl's .plan and .project files"
 
 @route('/')
-def hello_world():
-    return 'Hello from Bottle!'
+def home():
+    return homeMsg
+
+@route('/.plan')
+def plan():
+    planFile=open('../.plan','r')
+    planTxt = "<html><head><title>Omar Vasquez .plan</title></head><pre style='white-space: pre-wrap'>"+planFile.read()+"</pre></html>"
+    return planTxt
 
 application = default_app()
 
